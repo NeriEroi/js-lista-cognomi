1e0f5af869dabd6d66ea254ad6d0b374857dc474
Michele <micheleierniero@gmail.com>
2021-07-20 13:42:36 +0200

    aggiunta commenti

--- js/main.js
+++ js/main.js
@@ -8,10 +8,18 @@ var id_surname = prompt("Qual è il tuo cognome?");
 
 console.log(id_surname);
 
+
+// Inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+
 console.log(surnameList.push(id_surname));
+
+
+// Stampa la lista ordinata alfabeticamente
 
 console.log(surnameList.sort());
 
+
+// Ciclo for
 
 for (i = 1; i < surnameList.length; i++) {
     console.log()
