b1ea7cddec9276cbea1d93b302bd97e3eb6ab3ff
Michele <micheleierniero@gmail.com>
2021-07-20 14:01:27 +0200

    ciclo for

--- js/main.js
+++ js/main.js
@@ -22,5 +22,17 @@ console.log(surnameList.sort());
 // Ciclo for
 
 for (i = 1; i < surnameList.length; i++) {
-    console.log()
+    console.log(surnameList.indexOf(id_surname))
+}
+
+
+// Ciclo while
+
+var i = 1
+
+while (i < surnameList.length) {
+
+    console.log(surnameList.indexOf(id_surname))
+
+    i++
 }
