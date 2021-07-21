608f88b0255bfa2678745d84da342bfbd83115b8
Michele <micheleierniero@gmail.com>
2021-07-21 10:55:56 +0200

    uppercase

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 // Chiedi all’utente il cognome
 
-var surnameList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
-
-console.log(surnameList);
-
 var id_surname = prompt("Qual è il tuo cognome?");
 
 console.log(id_surname);
@@ -11,7 +7,15 @@ console.log(id_surname);
 
 // Inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 
-console.log(surnameList.push(id_surname));
+var surnameList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+console.log(surnameList);
+
+
+var id_surname_up = id_surname.charAt(0).toUpperCase() + id_surname.slice(1).toLowerCase
+
+
+surnameList.push(id_surname_up);
+console.log(surnameList)
 
 
 // Stampa la lista ordinata alfabeticamente
@@ -25,7 +29,7 @@ var i = 1
 
 while (i < surnameList.length) {
 
-    console.log(surnameList.indexOf(id_surname))
+    console.log(surnameList.indexOf(id_surname_up))
 
     i++
 }
